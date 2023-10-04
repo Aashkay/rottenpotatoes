2b8662f69d24e9b2bf9ad55ae109992177bda058
aashaykadakia <aashaykadakia@tamu.edu>
2023-10-04 12:21:36 -0500

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,11 @@ more_movies = [
     {:title => 'Nomadland', :rating => 'R',
       :release_date => '19-Feb-2021'},
     {:title => 'CODA', :rating => 'PG-13',
-      :release_date => '13-Aug-2021'}
+      :release_date => '13-Aug-2021'},
+    {:title => 'CODA', :rating => 'PG-13',
+      :release_date => '13-Aug-2021'},
+    {:title => 'Nemo', :rating => 'PG',
+      :release_date => '30-May-2003'}  
   ]
   
   more_movies.each do |movie|
